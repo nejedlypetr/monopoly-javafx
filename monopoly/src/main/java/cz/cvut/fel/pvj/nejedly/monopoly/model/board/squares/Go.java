e52dfbb979745fed4d12e675beda93dab107b7f2
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-03-18 17:20:25 +0100

    Add remaining squares

---
+++ monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/board/squares/Go.java
@@ -0,0 +1,14 @@
+package cz.cvut.fel.pvj.nejedly.monopoly.model.board.squares;
+
+public class Go extends Square {
+    private final int salary;
+    public Go(int position, int salary) {
+        super("Go", position);
+        this.salary = salary;
+    }
+
+    @Override
+    boolean isOwnable() {
+        return false;
+    }
+}
