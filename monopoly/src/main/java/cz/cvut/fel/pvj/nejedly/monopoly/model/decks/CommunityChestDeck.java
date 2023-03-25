8fcf60118a79f54943b2c85c99346c49b2289009
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-03-25 20:24:15 +0100

    Make Deck class that is extended by CommunityChestDeck class and ChanceDeck class

--- monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/decks/CommunityChestDeck.java
+++ monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/decks/CommunityChestDeck.java
@@ -1,36 +1,15 @@
 package cz.cvut.fel.pvj.nejedly.monopoly.model.decks;
 
-import java.util.Random;
+public class CommunityChestDeck extends Deck {
 
-public class CommunityChestDeck {
-    private static int currentTopCardIndex = 0;
-    private static final Card[] cards = {
-            new Card(CardType.COMMUNITY_CHEST, "card1"),
-            new Card(CardType.COMMUNITY_CHEST, "card2"),
-            new Card(CardType.COMMUNITY_CHEST, "card3"),
-            new Card(CardType.COMMUNITY_CHEST, "card4"),
-    };
-
-    private CommunityChestDeck() {}
-
-    public static Card[] shuffle() {
-        Random random = new Random();
-        for (int i = cards.length; i > 1; i--) {
-            swap(i - 1, random.nextInt(i));
-        }
-        return cards;
-    }
-
-    public static Card drawCard() {
-        Card topCard = cards[currentTopCardIndex];
-        currentTopCardIndex++;
-        if (currentTopCardIndex == cards.length) currentTopCardIndex = 0;
-        return topCard;
-    }
-
-    private static void swap(int i, int j) {
-        Card temp = cards[i];
-        cards[i] = cards[j];
-        cards[j] = temp;
+    public CommunityChestDeck() {
+        super(
+            new Card[] {
+                new Card(CardType.COMMUNITY_CHEST, "Dummy card1"),
+                new Card(CardType.COMMUNITY_CHEST, "Dummy card2"),
+                new Card(CardType.COMMUNITY_CHEST, "Dummy card3"),
+                new Card(CardType.COMMUNITY_CHEST, "Dummy card4"),
+            }
+        );
     }
 }
---
+++ monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/decks/Deck.java
@@ -0,0 +1,34 @@
+package cz.cvut.fel.pvj.nejedly.monopoly.model.decks;
+
+import java.util.Random;
+
+public class Deck {
+    private int currentTopCardIndex;
+    private final Card[] cards;
+
+    public Deck(Card[] cards) {
+        this.cards = cards;
+        currentTopCardIndex = 0;
+    }
+
+    public Card[] shuffle() {
+        Random random = new Random();
+        for (int i = cards.length; i > 1; i--) {
+            swap(i - 1, random.nextInt(i));
+        }
+        return cards;
+    }
+
+    public Card drawCard() {
+        Card topCard = cards[currentTopCardIndex];
+        currentTopCardIndex++;
+        if (currentTopCardIndex == cards.length) currentTopCardIndex = 0;
+        return topCard;
+    }
+
+    private void swap(int i, int j) {
+        Card temp = cards[i];
+        cards[i] = cards[j];
+        cards[j] = temp;
+    }
+}
