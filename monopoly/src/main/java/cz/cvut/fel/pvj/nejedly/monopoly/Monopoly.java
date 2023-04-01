bab3abc1576b5a5f12329ab2580c75ba0a78c149
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-04-01 10:36:56 +0200

    Add new Main class

--- monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/Monopoly.java
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fel.pvj.nejedly.monopoly;
-
-public class Monopoly {
-    public static void main(String[] args) {
-        System.out.println("Monopoly in progress...");
-
-    }
-}
