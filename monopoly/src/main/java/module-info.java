b53c7bec8b40b6c86d794247dacd9636d955b838
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-04-05 13:38:11 +0200

    Open images, sprites, stylesheets

--- monopoly/src/main/java/module-info.java
+++ monopoly/src/main/java/module-info.java
@@ -1,4 +1,9 @@
 module cz.cvut.fel.pvj.nejedly.monopoly {
     requires javafx.controls;
     exports cz.cvut.fel.pvj.nejedly.monopoly;
+    exports cz.cvut.fel.pvj.nejedly.monopoly.model.board.squares;
+
+    opens images;
+    opens sprites;
+    opens stylesheets;
 }
