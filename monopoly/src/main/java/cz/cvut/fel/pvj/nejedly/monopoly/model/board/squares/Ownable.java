ea824b39816ef8276dbfe5799e6a0eafa8893437
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-04-18 13:25:31 +0200

    Add getOwner(), setOwner()

--- monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/board/squares/Ownable.java
+++ monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/board/squares/Ownable.java
@@ -1,8 +1,12 @@
 package cz.cvut.fel.pvj.nejedly.monopoly.model.board.squares;
 
+import cz.cvut.fel.pvj.nejedly.monopoly.model.player.Player;
+
 public interface Ownable {
     int getRent();
     boolean isOwned();
     int getPurchasePrice();
+    Player getOwner();
+    void setOwner(Player player);
     boolean isHabitable();
 }
