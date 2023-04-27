7f57f051fd4179be4842ed4ed1b886459920d521
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-04-27 20:51:52 +0200

    Remove GetOutOfJailFreeCard class

--- monopoly/src/main/java/cz/cvut/fel/pvj/nejedly/monopoly/model/decks/cards/GetOutOfJailFreeCard.java
+++
@@ -1,8 +0,0 @@
-package cz.cvut.fel.pvj.nejedly.monopoly.model.decks.cards;
-
-public class GetOutOfJailFreeCard extends Card {
-
-    public GetOutOfJailFreeCard(CardType cardType, String text) {
-        super(cardType, text);
-    }
-}
