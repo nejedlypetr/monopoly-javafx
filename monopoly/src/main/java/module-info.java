622814d472301ec44d1a0ec3a32eae6f58d91b5e
Petr Nejedlý <nejedpe4@fel.cvut.cz>
2023-04-28 14:30:51 +0200

    Add json-simple

--- monopoly/src/main/java/module-info.java
+++ monopoly/src/main/java/module-info.java
@@ -1,5 +1,6 @@
 module cz.cvut.fel.pvj.nejedly.monopoly {
     requires javafx.controls;
+    requires json.simple;
     exports cz.cvut.fel.pvj.nejedly.monopoly;
     exports cz.cvut.fel.pvj.nejedly.monopoly.model.board.squares;
 
